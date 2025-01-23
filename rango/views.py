4bf30432408f99d60b4fee157fd472b6fd055409
Keiran Walker <2895682w@student.gla.ac.uk>
2025-01-23 19:20:57 +0000

    Category Form Added

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     # Database Query
@@ -40,3 +42,22 @@ def show_category(request, category_name_slug):
 
     # Render Response
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+
+            # If valid save to database
+            form.save(commit=True)
+            return redirect('/rango/')
+        
+        else:
+
+            # Print errors to terminal
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
